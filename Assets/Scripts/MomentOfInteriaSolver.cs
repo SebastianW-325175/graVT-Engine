800a52f181dae0d4e8de134408569c609444a0e0
SebastianW-325175 <325175@stud.umk.pl>
2026-09-12 23:23:43 +0200

    Pierwsz implementacja momentu obrotowego

---
+++ Assets/Scripts/MomentOfInteriaSolver.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public static class MomentOfInteriaSolver
+{
+    // TODO: This assumes a non-offset collider. Account for offsets using the parallel axes theorem
+    public static float Solve(Collider2D collider2D, float mass)
+    {
+        return collider2D switch
+        {
+            CircleCollider2D circleCollider2D => Solve(circleCollider2D, mass),
+            BoxCollider2D boxCollider2D => Solve(boxCollider2D, mass),
+            _ => 1
+        };
+    }
+
+    private static float Solve(CircleCollider2D circleCollider2D, float mass)
+    {
+        return (float)(0.5 * mass * Math.Pow(circleCollider2D.radius, 2));
+    }
+
+    private static float Solve(BoxCollider2D boxCollider2D, float mass)
+    {
+        return (float)(mass * (Math.Pow(boxCollider2D.size.y, 2) + Math.Pow(boxCollider2D.size.x, 2)) / 12.0);
+    }
+}
--- Assets/Scripts/SmallSpaceObject.cs
+++ Assets/Scripts/SmallSpaceObject.cs
@@ -3,20 +3,53 @@ using UnityEngine;
 
 public class TinyObject : MonoBehaviour
 {
-    [Tooltip("Object mass in kg")]
-    [SerializeField] private float mass;
+    private const float RadiansToDegrees = 180f / Mathf.PI;
+    
+    [Tooltip("Object mass in kg")] [SerializeField]
+    private float mass;
 
+    [Tooltip("Object moment of inertia in kg * m^2")] [SerializeField]
+    private float momentOfInertia;
+
+    [Tooltip("The collider of the object. This will be used to calculate it's moment of inertia")] [SerializeField]
+    private Collider2D collider = null;
+    
     private Vector2 _linearVelocity;
     private float _angularVelocity;
 
     private void FixedUpdate()
     {
         gameObject.transform.Translate(_linearVelocity * Time.fixedDeltaTime);
+        gameObject.transform.Rotate(0, 0, (_angularVelocity * Time.fixedDeltaTime * RadiansToDegrees));
     }
 
     public void ApplyForce(Vector2 force)
     {
-        // This is probably wrong
-        _linearVelocity += force * Time.fixedDeltaTime;
+        _linearVelocity += (force * Time.fixedDeltaTime) / mass;
+    }
+
+    public void ApplyTorque(float torque)
+    {
+        _angularVelocity += (torque / momentOfInertia) * Time.fixedDeltaTime;
+    }
+
+    private void Update()
+    {
+        ApplyTorque(1.0f);
+    }
+
+    private void Start()
+    {
+        // Calculate the moment of inertia based on the collider
+        momentOfInertia = MomentOfInteriaSolver.Solve(collider, mass);
+    }
+
+    private void Awake()
+    {
+        if (collider == null)
+        {
+            Debug.Log("Collider not set. Assigning automatically.");
+            collider = GetComponent<Collider2D>();
+        }
     }
 }
